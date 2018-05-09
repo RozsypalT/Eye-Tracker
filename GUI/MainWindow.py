7c7218485f99f9c6bc4ea3a068c25da267c9d9e4
pavlikl <37178825+pavlikl@users.noreply.github.com>
2018-05-09 10:30:41 +0200

    Add files via upload

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -5,19 +5,21 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-from ChooserWindow import Ui_ChooserWindow
-from PreviewWindow import Ui_PreviewWindow
 
 class Ui_MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.title = 'Image Chooser'
-        self.width = 1000
-        self.height = 800
+        screen = app.primaryScreen()
+        size = screen.size()
+        self.window_width = int(size.width() / 1.75)
+        self.window_height = int((3.5 * size.height()) / 5)
         self.selected = []
         self.imgcountlabel = QLabel(self)
         self.comboboxlabel = QLabel(self)
+        self.gallerylabel = QLabel(self)
+        self.countlabel = QLabel(self)
         self.centralwidget = QtWidgets.QWidget(self)
         self.addbutton = QtWidgets.QPushButton(self.centralwidget)
         self.removebutton = QtWidgets.QPushButton(self.centralwidget)
@@ -36,56 +38,58 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.gallerygrid.setRowStretch(1, 10)
         self.gallerygrid.setRowStretch(2, 10)
         self.labellist = []
-        self.testlabel = QLabel(self)
         self.highlighted = None
         self.i = 0
         self.j = 0
         self.x = 3
         self.y = 3
-        self.chooserWin = Ui_ChooserWindow(self)
-        self.previewWin = Ui_PreviewWindow(self)
         self.setupUi(self)
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1000, 800)
-       # MainWindow.setFixedSize(1000, 800)
+
+        MainWindow.setFixedSize(self.window_width, self.window_height)
+        MainWindow.setWindowTitle(self.title)
 
         self.centralwidget.setObjectName("centralwidget")
 
-        self.comboboxlabel.setGeometry(QtCore.QRect(50,150,100,32))
+        self.gallerylabel.setGeometry(QtCore.QRect(int(self.window_width/4.5), int(0.5*self.window_height/15), int(self.window_width/10), int(self.window_height/25)))
+        self.gallerylabel.setText("Selected pictures:")
+
+        self.imgcountlabel.setGeometry(QtCore.QRect(int(self.window_width / 4.5), int(12.4 * self.window_height / 15), int(self.window_width),int(self.window_height / 25)))
+        self.imgcountlabel.setText("Number of selected pictures: " + str(len(self.selected)) + ". " + str(self.x*self.y - len(self.selected)) + " more pictures needed.")
+
+        self.comboboxlabel.setGeometry(QtCore.QRect(int(self.window_width/15), int(2.5*self.window_height/15), int(self.window_width/10), int(self.window_height/25)))
         self.comboboxlabel.setText("Layout:")
 
         self.addbutton.setText("Add Pictures")
-        self.addbutton.setGeometry(QtCore.QRect(50, 50, 100, 32))
+        self.addbutton.setGeometry(QtCore.QRect(int(self.window_width/15), int(self.window_height/15), int(self.window_width/10), int(self.window_height/25)))
         self.addbutton.clicked.connect(self.addPictures)
         self.addbutton.setObjectName("pushButton")
 
         self.removebutton.setText("Remove")
-        self.removebutton.setGeometry(QtCore.QRect(50, 114, 100, 32))
+        self.removebutton.setGeometry(QtCore.QRect(int(self.window_width/15), int(2*(self.window_height/15)), int(self.window_width/10), int(self.window_height/25)))
         self.removebutton.clicked.connect(self.removePicture)
         self.removebutton.setObjectName("pushButton_2")
 
         self.previewbutton.setText("Preview")
-        self.previewbutton.setGeometry(QtCore.QRect(50, 242, 100, 32))
-        self.previewbutton.clicked.connect(self.startPreviewProcess)
+        self.previewbutton.setGeometry(QtCore.QRect(int(self.window_width/15), int(4*(self.window_height/15)), int(self.window_width/10), int(self.window_height/25)))
         self.previewbutton.setObjectName("pushButton_3")
 
         self.helpbutton.setText("Help")
-        self.helpbutton.setGeometry(QtCore.QRect(50, 308, 100, 32))
-        self.helpbutton.clicked.connect(self.startImageChooserProcess)
+        self.helpbutton.setGeometry(QtCore.QRect(int(self.window_width/15), int(5*(self.window_height/15)), int(self.window_width/10), int(self.window_height/25)))
         self.helpbutton.setObjectName("pushButton_4")
 
         self.startbutton.setText("Start")
-        self.startbutton.setGeometry(QtCore.QRect(375, self.height-82, 100, 32))
+        self.startbutton.setGeometry(QtCore.QRect(int(self.window_width/3), int(13.5*(self.window_height/15)), int(self.window_width/10), int(self.window_height/25)))
         self.startbutton.setObjectName("pushButton_4")
 
         self.quitbutton.setText("Quit")
-        self.quitbutton.setGeometry(QtCore.QRect(525, self.height-82, 100, 32))
+        self.quitbutton.setGeometry(QtCore.QRect(int(self.window_width/1.75), int(13.5*(self.window_height/15)), int(self.window_width/10), int(self.window_height/25)))
         self.quitbutton.clicked.connect(self.quit)
         self.quitbutton.setObjectName("pushButton_4")
 
-        self.layoutbox.setGeometry(QtCore.QRect(50, 178, 100, 32))
+        self.layoutbox.setGeometry(QtCore.QRect(int(self.window_width/15), int(3*self.window_height/15), int(self.window_width/10), int(self.window_height/25)))
         self.layoutbox.setObjectName("layout")
         self.layoutbox.addItem("3x3")
         self.layoutbox.addItem("3x2")
@@ -93,12 +97,15 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.layoutbox.addItem("2x2")
         self.layoutbox.currentTextChanged.connect(self.selection)
 
-        self.gallery.setGeometry(QtCore.QRect(187, 47, 603, 621))
+        self.gallery.setGeometry(QtCore.QRect(int(self.window_width/4.5), int(self.window_height/15), int((self.window_width/1.75)), int((self.window_height/1.3))))
+        print(int((self.window_width/1.75)))
+        print(int((self.window_height / 1.3)))
         self.gallery.setWidgetResizable(True)
-        self.gallery.setFixedWidth(620)
+        self.gallery.setFixedWidth(int((self.window_width/1.75)))
         self.gallery.setObjectName("gallery")
-
-        self.gallerycontents.setGeometry(QtCore.QRect(0, 0, 620, 618))
+        print(str((self.window_width / 1.75) / 3.75))
+        print(str((self.window_height / 1.3) / 3.5))
+        self.gallerycontents.setGeometry(QtCore.QRect(0, 0, int((self.window_width/1.75)), int((self.window_height/1.3))))
         self.gallerycontents.setObjectName("scrollAreaWidgetContents")
 
         self.gallerygrid.setObjectName("gridLayout")
@@ -108,11 +115,6 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         MainWindow.setCentralWidget(self.centralwidget)
 
 
-    def selection(self, value):
-        str_res = value.split("x")
-        self.x = int(str_res[0])
-        self.y = int(str_res[1])
-
     def addPictures(self):
         file = QFileDialog.getOpenFileNames(self, "QFileDialog.getOpenFileName()", "",
                                                "Image Files (*.png *.jpg)")
@@ -121,6 +123,29 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
 
             filename = file[0][l]
             self.loadPicture(filename)
+
+        self.imgcountlabel.setText("Number of selected pictures: " + str(len(self.selected)) + ". " + str(
+            self.x * self.y - len(self.selected)) + " more pictures needed.")
+
+    def loadPicture(self, filename):
+        if filename == '':
+            return
+        self.selected.append(filename)
+        label = QLabel(self.gallerycontents)
+        pixmap = QPixmap(filename)
+        label.setFixedSize(int((self.window_width / 1.75) / 3.25), int((self.window_height / 1.3) / 3.175))
+        modpixmap = pixmap.scaled(int((self.window_width/1.75)/3.25)-15, int((self.window_height/1.3)/3.175)-15)
+        label.setPixmap(modpixmap)
+        label.setAlignment(Qt.AlignCenter)
+        label.setStyleSheet("background-color: white")
+        label.mousePressEvent = lambda event: self.highlight(event, label)
+        self.labellist.append(label)
+        self.gallerygrid.addWidget(label, self.j, self.i)
+        self.i = self.i + 1
+
+        if self.i == 3:
+            self.i = 0
+            self.j = self.j + 1
 
     def removePicture(self):
         k = 0
@@ -147,50 +172,31 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
             lab.show()
         self.i = k
         self.j = l
+        self.imgcountlabel.setText("Number of selected pictures: " + str(len(self.selected)) + ". " + str(
+            self.x * self.y - len(self.selected)) + " more pictures needed.")
+
+    def selection(self, value):
+        str_res = value.split("x")
+        self.x = int(str_res[0])
+        self.y = int(str_res[1])
+        self.imgcountlabel.setText("Number of selected pictures: " + str(len(self.selected)) + ". " + str(
+            self.x * self.y - len(self.selected)) + " more pictures needed.")
 
     def highlight(self, event, label):
         if self.highlighted is None:
             label.setStyleSheet("border: 5px inset red")
             self.highlighted = label
         elif self.highlighted == label:
-            label.setStyleSheet("border: 5px inset white")
+            label.setStyleSheet("background-color: white")
             self.highlighted = None
         else:
-            self.highlighted.setStyleSheet("border: 5px inset white")
+            self.highlighted.setStyleSheet("background-color: white")
             self.highlighted = label
             label.setStyleSheet("border: 5px inset red")
-
-    def loadPicture(self, filename):
-        if filename == '':
-            return
-        self.selected.append(filename)
-        label = QLabel(self.gallerycontents)
-        pixmap = QPixmap(filename)
-        label.setFixedSize(190, 190)
-        modpixmap = pixmap.scaled(180, 180)
-        label.setPixmap(modpixmap)
-        label.setAlignment(Qt.AlignCenter)
-        label.setStyleSheet("background-color: white")
-        label.mousePressEvent = lambda event: self.highlight(event, label)
-        self.labellist.append(label)
-        self.gallerygrid.addWidget(label, self.j, self.i)
-        self.i = self.i + 1
-
-        if self.i == 3:
-            self.i = 0
-            self.j = self.j + 1
 
     def quit(self):
         self.close()
 
-    def startImageChooserProcess(self):
-        self.chooserWin.show()
-        self.hide()
-
-    def startPreviewProcess(self):
-        self.hide()
-        self.previewWin.show()
-        self.previewWin.loadImages(self.selected, self.x, self.y)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
